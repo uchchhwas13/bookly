ff36408898141534dbaac35b88c85efdeb433a61
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-09-23 00:33:58 +0600

    Adding JWT_SECRET_KEY and JWT_ALGORITHM in config

--- src/config.py
+++ src/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str = ""
+    JWT_SECRET_KEY: str = ""
+    JWT_ALGORITHM: str = ""
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore"
