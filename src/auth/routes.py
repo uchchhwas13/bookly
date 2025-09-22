28aa03b4fb5d924fe12587b91b205960990a6c1d
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-09-23 00:34:30 +0600

    Adding route for login_user

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from .schemas import UserCreateModel, UserModel
+from .schemas import LoginResponse, UserCreateModel, UserModel, UserLoginModel, UserResponse
 from .service import AuthService
 from src.db.main import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
-
+from .utils import create_access_token, verify_password, create_refresh_token
 
 auth_router = APIRouter()
 auth_service = AuthService()
@@ -20,3 +20,40 @@ async def create_user_account(user_data: UserCreateModel, session: AsyncSession 
 
     new_user = await auth_service.create_user(user_data, session)
     return new_user
+
+
+@auth_router.post('/login', response_model=LoginResponse, status_code=status.HTTP_200_OK)
+async def login_user(login_data: UserLoginModel, session: AsyncSession = Depends(get_session)):
+    email = login_data.email
+    password = login_data.password
+
+    user = await auth_service.get_user_by_email(email, session)
+
+    if user is not None:
+        password_valid = verify_password(password, user.password_hash)
+        if password_valid:
+            access_token = create_access_token(user_data={
+                'email': user.email,
+                'user_uid': str(user.uid)
+            })
+
+            refresh_token = create_refresh_token(
+                user_data={
+                    'email': user.email,
+                    'user_uid': str(user.uid)
+                }
+            )
+            return LoginResponse(
+                message="Login successful",
+                access_token=access_token,
+                refresh_token=refresh_token,
+                user=UserResponse(
+                    email=user.email,
+                    uid=str(user.uid)
+                )
+            )
+
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="Invalid Email or Password"
+    )
