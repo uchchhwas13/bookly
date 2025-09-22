1b4f18817b276bab3faa7329aae8a24f402f766a
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-09-22 13:37:52 +0600

    Defining User model

---
+++ src/auth/models.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+import uuid
+from sqlalchemy.sql import func
+
+
+class User(SQLModel, table=True):
+    __tablename__ = 'users'
+
+    uid: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            nullable=False,
+            primary_key=True,
+            default=uuid.uuid4
+        )
+    )
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    is_verified: bool = False
+    created_at: datetime = Field(sa_column=Column(
+        pg.TIMESTAMP, server_default=func.now()))
+    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, server_default=func.now(),
+                                                  onupdate=func.now()))
