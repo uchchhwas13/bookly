266d6de4edec6cb3a5af9d8bf35c859773dbd516
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-09-23 16:20:11 +0600

    Adding REDIS_HOST and port

--- src/config.py
+++ src/config.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     JWT_ACCESS_TOKEN_SECRET_KEY: str = ""
     JWT_REFRESH_TOKEN_SECRET_KEY: str = ""
     JWT_ALGORITHM: str = ""
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore"
