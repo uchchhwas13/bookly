be794136ad61a7cd42b2994836370b371f89a88c
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-09-23 10:47:03 +0600

    Adding separate methods for verifying accessToken and refreshToken

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,5 +1,6 @@
+from fastapi import HTTPException, status
 from datetime import datetime, timedelta
-from typing import Any
+from typing import Any, Optional
 import uuid
 import jwt
 from passlib.context import CryptContext
@@ -23,13 +24,13 @@ def create_access_token(user_data: dict[str, str]) -> str:
     payload: dict[str, Any] = {
         "user": user_data,
         "exp": datetime.now() + timedelta(seconds=ACCESS_TOKEN_EXPIRY_DURATION),
-        "refresh": False,
+        "type": "access",
         "jti": str(uuid.uuid4())
     }
 
     token = jwt.encode(
         payload,
-        key=config.JWT_SECRET_KEY,
+        key=config.JWT_ACCESS_TOKEN_SECRET_KEY,
         algorithm=config.JWT_ALGORITHM,
     )
     return token
@@ -39,25 +40,62 @@ def create_refresh_token(user_data: dict[str, str]) -> str:
     payload: dict[str, Any] = {
         "user": user_data,
         "exp": datetime.now() + timedelta(seconds=REFRESH_TOKEN_EXPIRY_DURATION),
-        "refresh": True,
+        "type": "refresh",
         "jti": str(uuid.uuid4())
     }
     token: str = jwt.encode(
         payload,
-        key=config.JWT_SECRET_KEY,
+        key=config.JWT_REFRESH_TOKEN_SECRET_KEY,
         algorithm=config.JWT_ALGORITHM,
     )
     return token
 
 
-def decode_token(token: str) -> dict[str, Any] | None:
+def verify_access_token(token: str) -> Optional[dict[str, Any]]:
     try:
-        token_data: dict[str, Any] = jwt.decode(
-            jwt=token,
-            key=config.JWT_SECRET_KEY,
-            algorithms=[config.JWT_ALGORITHM],
+        payload: dict[str, Any] = jwt.decode(
+            token,
+            key=config.JWT_ACCESS_TOKEN_SECRET_KEY,
+            algorithms=[config.JWT_ALGORITHM]
         )
-        return token_data
-    except jwt.PyJWTError as error:
-        logging.exception(error)
-        return None
+        if payload.get("type") != "access":
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid access token"
+            )
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Access token expired"
+        )
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid access token"
+        )
+
+
+def verify_refresh_token(token: str) -> Optional[dict[str, Any]]:
+    try:
+        payload: dict[str, Any] = jwt.decode(
+            token,
+            key=config.JWT_ACCESS_TOKEN_SECRET_KEY,
+            algorithms=[config.JWT_ALGORITHM]
+        )
+        if payload.get("type") != "refresh":
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid refresh token"
+            )
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Refresh token expired"
+        )
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid refresh token"
+        )
