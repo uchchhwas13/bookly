37a2efa71f65567958e9b5d4bd9f9e195a202b40
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-09-25 11:26:41 +0600

    Moving access_token_bearer dependency into router level

--- src/routes/book_routes.py
+++ src/routes/book_routes.py
@@ -8,16 +8,15 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from ..services.book_service import BookService
 from ..schemas.book import BookCreateModel
 from src.dependencies import AccessTokenBearer, get_current_user_from_token
-from fastapi.security import HTTPAuthorizationCredentials
 
-book_router = APIRouter()
+
 book_service = BookService()
 access_token_bearer = AccessTokenBearer()
+book_router = APIRouter(dependencies=[Depends(access_token_bearer)])
 
 
 @book_router.get('/', response_model=List[BookDetailsModel], status_code=status.HTTP_200_OK)
 async def get_books(session: Annotated[AsyncSession, Depends(get_session)],
-                    _: Annotated[HTTPAuthorizationCredentials, Depends(access_token_bearer)],
                     user_uid: str | None = None):
     if user_uid:
         return await book_service.get_user_books(user_uid, session)
@@ -32,7 +31,7 @@ async def create_a_book(book_data: BookCreateModel, session: Annotated[AsyncSess
 
 
 @book_router.get('/{book_uid}', response_model=BookDetailsModel)
-async def get_book(book_uid: str, session: Annotated[AsyncSession, Depends(get_session)],  _: Annotated[HTTPAuthorizationCredentials, Depends(access_token_bearer)]):
+async def get_book(book_uid: str, session: Annotated[AsyncSession, Depends(get_session)]):
     book = await book_service.get_book(book_uid, session)
 
     if book:
@@ -43,7 +42,7 @@ async def get_book(book_uid: str, session: Annotated[AsyncSession, Depends(get_s
 
 
 @book_router.patch('/{book_uid}', response_model=BookModel)
-async def update_book(book_uid: str, book_update_data: BookUpdateModel, session: Annotated[AsyncSession, Depends(get_session)],  _: Annotated[HTTPAuthorizationCredentials, Depends(access_token_bearer)]):
+async def update_book(book_uid: str, book_update_data: BookUpdateModel, session: Annotated[AsyncSession, Depends(get_session)]):
     updated_book = await book_service.update_book(book_uid, book_update_data, session)
     if updated_book:
         return updated_book
@@ -53,7 +52,7 @@ async def update_book(book_uid: str, book_update_data: BookUpdateModel, session:
 
 
 @book_router.delete('/{book_uid}', response_model=BookModel)
-async def delete_book(book_uid: str, session: Annotated[AsyncSession, Depends(get_session)],  _: Annotated[HTTPAuthorizationCredentials, Depends(access_token_bearer)]):
+async def delete_book(book_uid: str, session: Annotated[AsyncSession, Depends(get_session)]):
     deleted_book = await book_service.delete_book(book_uid, session)
 
     if deleted_book:
